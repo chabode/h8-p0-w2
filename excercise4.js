21cecb0a767fb039a51a4bcec2128c1d92c8187f
chabode <rizqa.aulia@gmail.com>
2020-02-25 16:44:07 +0700

    3rd commit

--- excercise4.js
+++ excercise4.js
@@ -4,29 +4,28 @@ var tahun = 2012
 
 switch(bulan){
     case 1 : console.log(`${tanggal} Januari ${tahun}`)
-    // bulan = "Januari"
     break
-    case 2 : bulan = "Februari"
+    case 2 : console.log(`${tanggal} Februari ${tahun}`)
     break
-    case 3 : bulan = "Maret"
+    case 3 : console.log(`${tanggal} Maret ${tahun}`)
     break
-    case 4 : bulan = "April"
+    case 4 : console.log(`${tanggal} April ${tahun}`)
     break
-    case 5 : bulan = "Mei"
+    case 5 : console.log(`${tanggal} Mei ${tahun}`)
     break
-    case 6 : bulan = "Juni"
+    case 6 : console.log(`${tanggal} Juni ${tahun}`)
     break
-    case 7 : bulan = "Juli"
+    case 7 : console.log(`${tanggal} Juli ${tahun}`)
     break
-    case 8 : bulan = "Agustus"
+    case 8 : console.log(`${tanggal} Agustus ${tahun}`)
     break
-    case 9 : bulan = "September"
+    case 9 : console.log(`${tanggal} September ${tahun}`)
     break
-    case 10 : bulan = "Oktober"
+    case 10 : console.log(`${tanggal} Oktober ${tahun}`)
     break
-    case 11 : bulan = "November"
+    case 11 : console.log(`${tanggal} November ${tahun}`)
     break
-    case 12 : bulan = "Desember"
+    case 12 : console.log(`${tanggal} Desember ${tahun}`)
     break
     default : console.log("Bulan melewati batas")
     break
