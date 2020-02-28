0a6c18eb7f30c4caa0a4d0149e2bed90143896cc
chabode <rizqa.aulia@gmail.com>
2020-02-28 17:20:59 +0700

    tambahan update

--- exercise6.js
+++ exercise6.js
@@ -1,56 +1,56 @@
 // 1. Melakukan looping menggunakan while
 
-// var angka = 1
-// console.log('Looping Pertama')
-// while (angka<21){
-//     if (angka % 2 === 0)
-//          console.log(angka + ' - I love coding')
-//     angka++
-// }
-// var angka2 = 20
-// console.log('Looping Kedua')
-// while (angka2 > 0){
-//     if (angka2 % 2 === 0)
-//          console.log(angka2 + ' - I will become fullstack developer')
-//     angka2--
-// }
+var angka = 1
+console.log('Looping Pertama')
+while (angka<21){
+    if (angka % 2 === 0)
+         console.log(angka + ' - I love coding')
+    angka++
+}
+var angka2 = 20
+console.log('Looping Kedua')
+while (angka2 > 0){
+    if (angka2 % 2 === 0)
+         console.log(angka2 + ' - I will become fullstack developer')
+    angka2--
+}
 
 // 2. Melakukan looping menggunakan for
 
-// console.log('Looping Pertama')
-// for (var angka3 = 1 ; angka3 <= 20; angka3++){
-//     console.log(angka3 + ' - I love coding')
-// }
-// console.log('Looping Kedua')
-// for (var angka4 = 20; angka4 > 0; angka4--){
-//     console.log(angka4 + ' - I will become fullstack developer')
-// }
+console.log('Looping Pertama')
+for (var angka3 = 1 ; angka3 <= 20; angka3++){
+    console.log(angka3 + ' - I love coding')
+}
+console.log('Looping Kedua')
+for (var angka4 = 20; angka4 > 0; angka4--){
+    console.log(angka4 + ' - I will become fullstack developer')
+}
 
 // 3. Angka Ganjil dan Genap
 
-// for (var angka5 = 1; angka5 <= 100; angka5++){
-//     if (angka5 % 2 !== 0) {
-//         console.log("Ganjil")//: " + angka5)
-//     } else {
-//         console.log("Genap")//: " + angka5)
-//     }
-// }
-// for (var angka6 = 1; angka6 <= 100; angka6+=2){
-//     if (angka6 % 3 === 0){
-//         console.log(angka6 + ' Kelipatan 3')
-//     } else {
-//         // console.log(angka6)
-//         // console.log("")
-//     }
-// }
-// for (var angka7 = 1; angka7 <= 100; angka7+=5){
-//     if (angka7 % 6 === 0){
-//         console.log(angka7 + ' Kelipatan 6')
-//     } else {
-//         // console.log(angka7)
-//         // console.log("")
-//     }
-// }
+for (var angka5 = 1; angka5 <= 100; angka5++){
+    if (angka5 % 2 !== 0) {
+        console.log("Ganjil")//: " + angka5)
+    } else {
+        console.log("Genap")//: " + angka5)
+    }
+}
+for (var angka6 = 1; angka6 <= 100; angka6+=2){
+    if (angka6 % 3 === 0){
+        console.log(angka6 + ' Kelipatan 3')
+    } else {
+        // console.log(angka6)
+        // console.log("")
+    }
+}
+for (var angka7 = 1; angka7 <= 100; angka7+=5){
+    if (angka7 % 6 === 0){
+        console.log(angka7 + ' Kelipatan 6')
+    } else {
+        // console.log(angka7)
+        // console.log("")
+    }
+}
 for (var angka8 = 1; angka8 <= 100; angka8+=9){
     if (angka8 % 10 === 0){
         console.log(angka8 + ' Kelipatan 10')
